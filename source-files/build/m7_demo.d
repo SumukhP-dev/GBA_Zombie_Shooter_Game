21be36747c2a2a987357e4c231421b5590aa8c5d
SumukhP-dev <sumukh.paspuleti@gmail.com>
2025-08-14 22:00:18 -0400

    Add advanced example game

---
+++ source-files/build/m7_demo.d
@@ -0,0 +1,36 @@
+m7_demo.o: \
+ C:/Users/sumukh/Documents/GBA_Zombie_Shooter_Game/libtonc-examples/adv/m7_demo/source/m7_demo.c \
+ C:/devkitPro/libtonc/include/tonc.h \
+ C:/devkitPro/libtonc/include/tonc_types.h \
+ C:/devkitPro/libtonc/include/tonc_memmap.h \
+ C:/devkitPro/libtonc/include/tonc_memdef.h \
+ C:/devkitPro/libtonc/include/tonc_bios.h \
+ C:/devkitPro/libtonc/include/tonc_core.h \
+ C:/devkitPro/libtonc/include/tonc_input.h \
+ C:/devkitPro/libtonc/include/tonc_irq.h \
+ C:/devkitPro/libtonc/include/tonc_math.h \
+ C:/devkitPro/libtonc/include/tonc_oam.h \
+ C:/devkitPro/libtonc/include/tonc_tte.h \
+ C:/devkitPro/libtonc/include/tonc_surface.h \
+ C:/devkitPro/libtonc/include/tonc_video.h \
+ C:/devkitPro/libtonc/include/tonc_nocash.h \
+ C:/devkitPro/libtonc/include/tonc_text.h \
+ C:/Users/sumukh/Documents/GBA_Zombie_Shooter_Game/libtonc-examples/adv/m7_demo/source/m7_demo.h \
+ C:/Users/sumukh/Documents/GBA_Zombie_Shooter_Game/libtonc-examples/adv/m7_demo/build/nums.h
+C:/devkitPro/libtonc/include/tonc.h:
+C:/devkitPro/libtonc/include/tonc_types.h:
+C:/devkitPro/libtonc/include/tonc_memmap.h:
+C:/devkitPro/libtonc/include/tonc_memdef.h:
+C:/devkitPro/libtonc/include/tonc_bios.h:
+C:/devkitPro/libtonc/include/tonc_core.h:
+C:/devkitPro/libtonc/include/tonc_input.h:
+C:/devkitPro/libtonc/include/tonc_irq.h:
+C:/devkitPro/libtonc/include/tonc_math.h:
+C:/devkitPro/libtonc/include/tonc_oam.h:
+C:/devkitPro/libtonc/include/tonc_tte.h:
+C:/devkitPro/libtonc/include/tonc_surface.h:
+C:/devkitPro/libtonc/include/tonc_video.h:
+C:/devkitPro/libtonc/include/tonc_nocash.h:
+C:/devkitPro/libtonc/include/tonc_text.h:
+C:/Users/sumukh/Documents/GBA_Zombie_Shooter_Game/libtonc-examples/adv/m7_demo/source/m7_demo.h:
+C:/Users/sumukh/Documents/GBA_Zombie_Shooter_Game/libtonc-examples/adv/m7_demo/build/nums.h:
